4eef50d6d1bacbcfb6a01c8aad4dda9da998c433
Tim Buckley <timothypbuckley@gmail.com>
2015-10-11 16:18:35 -0400

    Add Variable struct.

--- ast.rb
+++ ast.rb
@@ -1,5 +1,24 @@
 # utf 8
 
+class Variable < Struct.new(:name)
+    def to_s
+        name.to_s
+    end
+
+    def inspect
+        "«#{self}»"
+    end
+
+    def reducible?
+        true
+    end
+
+    def reduce(environment)
+        environment[name]
+    end
+end
+
+
 class Number < Struct.new(:value)
     def to_s
         value.to_s
